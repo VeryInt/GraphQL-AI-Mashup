deb7557e4c01bc5857b014c881b2837ccfa32665
loo-y <loo-y@outlook.com>
2024-03-23 23:51:48 +0800

    fix: try edge runtime again

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { schema } from './schema'
 import { useDeferStream } from '@graphql-yoga/plugin-defer-stream'
 
 // vercel edge runtime
-// export const config = {
-//     runtime: 'edge',
-// }
+export const runtime = 'edge'
 
 function main() {
     const yoga = createYoga({
