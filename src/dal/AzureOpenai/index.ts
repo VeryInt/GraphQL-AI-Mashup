4c54caa9aec38dfc28f0df7706e9720c2f4023d8
loo-y <loo-y@outlook.com>
2024-04-30 23:56:55 +0800

    feat: add tool calling of internet search for azure stream

--- src/dal/AzureOpenai/index.ts
+++ src/dal/AzureOpenai/index.ts
@@ -12,7 +12,8 @@ const availableFunctions: Record<string, any> = {
     get_internet_serch_result: getInternetSerchResult,
 }
 
-const DEFAULT_MODEL_NAME = `gpt-35-turbo` //'gpt-4'// 'gpt-35-turbo' // deploymentId
+// const DEFAULT_MODEL_NAME = `gpt-35-turbo`  // deploymentId
+const DEFAULT_MODEL_NAME = `gpt-4`
 
 const convertMessages = (messages: ICommonDalArgs['messages']): { history: IMessage[] } => {
     let history = _.map(messages, message => {
@@ -60,26 +61,120 @@ const fetchAzureOpenai = async (ctx: TBaseContext, params: Record<string, any>, 
 
     if (isStream) {
         try {
-            const completion = await client.streamChatCompletions(modelUse, history, {
-                maxTokens: max_tokens,
-            })
-
             let content = ``
-            for await (const chunk of completion) {
-                const text = chunk.choices?.[0]?.delta?.content || ``
-                console.log(`Azure Openai text`, text)
-                if (text) {
+            if (searchWeb) {
+                const firstRoundCompletion = await client.streamChatCompletions(modelUse, history, {
+                    maxTokens: max_tokens,
+                    toolChoice: 'auto',
+                    tools,
+                })
+                let toolCalls: Record<string, any>[] = []
+
+                for await (const chunk of firstRoundCompletion) {
+                    const delta = chunk.choices?.[0]?.delta
+                    const toolCallsChunk = delta?.toolCalls
+                    const text = delta?.content || ``
+                    if (text) {
+                        streamHandler({
+                            token: text,
+                            status: true,
+                        })
+                        content += text
+                    } else if (toolCallsChunk && !_.isEmpty(toolCallsChunk)) {
+                        for (const toolCallChunk of toolCallsChunk as ChatCompletionsFunctionToolCall[]) {
+                            const toolCallId = toolCallChunk?.id
+                            const toolCallIndex = toolCallChunk?.index || 0
+                            const { name: functionName, arguments: funArgs } = toolCallChunk.function || {}
+                            if (toolCallId && functionName) {
+                                toolCalls[toolCallIndex] = {
+                                    id: toolCallId,
+                                    function: {
+                                        name: functionName,
+                                    },
+
+                                    type: 'function',
+                                }
+                            } else if (funArgs) {
+                                toolCalls[toolCallIndex] = {
+                                    ...toolCalls[toolCallIndex],
+                                    function: {
+                                        ...toolCalls[toolCallIndex]?.function,
+                                        arguments: (toolCalls[toolCallIndex]?.function?.arguments || '') + funArgs,
+                                    },
+                                }
+                            }
+                        }
+                    }
+                }
+
+                history.push({
+                    // @ts-ignore
+                    content: null,
+                    // @ts-ignore
+                    role: 'assistant',
+                    toolCalls: toolCalls,
+                })
+
+                for (const toolCall of toolCalls) {
+                    const { name: functionName, arguments: funArgs } = toolCall.function || {}
+                    const functionToCall = availableFunctions[functionName]
+                    const functionArgs = JSON.parse(funArgs?.match(/\{(?:[^{}]*)*\}/g)?.[0] || '{}')
                     streamHandler({
-                        token: text,
+                        token: `正在搜索 ${functionArgs.searchText}...\n\n`,
                         status: true,
                     })
-                    content += text
+
+                    const functionResponse = await functionToCall(functionArgs.searchText, functionArgs.count)
+                    history.push({
+                        toolCallId: toolCall.id,
+                        // @ts-ignore
+                        role: 'tool',
+                        name: functionName,
+                        content: functionResponse,
+                    })
                 }
+
+                const completion = await client.streamChatCompletions(modelUse, history, {
+                    maxTokens: max_tokens,
+                })
+                for await (const chunk of completion) {
+                    const text = chunk.choices?.[0]?.delta?.content || ``
+                    console.log(`Azure Openai text`, text)
+                    if (text) {
+                        streamHandler({
+                            token: text,
+                            status: true,
+                        })
+                        content += text
+                    }
+                }
+                completeHandler({
+                    content: content,
+                    status: true,
+                })
+                console.log(`🐹🐹🐹 completed content`, content)
+            } else {
+                const completion = await client.streamChatCompletions(modelUse, history, {
+                    maxTokens: max_tokens,
+                })
+
+                for await (const chunk of completion) {
+                    const text = chunk.choices?.[0]?.delta?.content || ``
+                    console.log(`Azure Openai text`, text)
+                    if (text) {
+                        streamHandler({
+                            token: text,
+                            status: true,
+                        })
+                        content += text
+                    }
+                }
+                completeHandler({
+                    content: content,
+                    status: true,
+                })
+                console.log(`🐹🐹🐹 completed content`, content)
             }
-            completeHandler({
-                content: content,
-                status: true,
-            })
         } catch (e) {
             console.log(`Azure Openai error`, e)
 
@@ -99,6 +194,7 @@ const fetchAzureOpenai = async (ctx: TBaseContext, params: Record<string, any>, 
                 })
                 const firstRoundMessage = firstRoundResult?.choices?.[0]?.message
                 if (firstRoundMessage?.toolCalls && !_.isEmpty(firstRoundMessage.toolCalls)) {
+                    console.log(`firstRoundMessage`, firstRoundMessage)
                     // @ts-ignore
                     history.push(firstRoundMessage)
                     for (const toolCall of firstRoundMessage.toolCalls as ChatCompletionsFunctionToolCall[]) {
